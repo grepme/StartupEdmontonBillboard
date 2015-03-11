78541d439323fe9a7f722292ecb446b160edc8cb
Kyle Richelhoff <kyle@mythcomputers.ca>
2015-03-11 15:46:42 -0600

    Lots of cleanup and documentation

---
+++ README.md
@@ -0,0 +1,15 @@
+Startup Edmonton Billboard
+==========================
+
+Welcome to the billboard that runs on the touch screen at Startup Edmonton!
+
+It lists events by day and let's you interact with them for more information. 
+
+It displays the name of the event, the location in the office, and the time it is being conducted. All imported
+from Google Calendar.
+
+It updates every 10 seconds for informative information on events going on.
+
+The goal is to build an interactive medium with touch features that can communicate to members what's going on.
+It is currently in Beta and is expected to grow in size and modules. This was a simple hack project done in several
+hours to originally run on a Raspberry Pi.
--- StartupEdmontonBillBoard.py
+++ StartupEdmontonBillBoard.py
@@ -1,57 +1,87 @@
-from flask import Flask
-from flask import render_template
+"""
+Welcome to the Startup Edmonton Billboard!
+
+
+---------------
+- Quick Start -
+---------------
+To run:
+    python StartupEdmontonBillBoard.py
+
+Open in your browser and go to:
+    http://127.0.0.1:5000/
+
+You will need to configure settings.py
+You will notice a settings_example.py of all the settings that need configuring.
+Rename that file to settings.py
+
+No other options are currently available.
+"""
+
+# Common imports
+from flask import Flask, render_template
 import requests
 from requests.exceptions import ConnectionError
 import datetime
 
+# Any additional settings
 from settings import API_KEY, CALENDAR_ID
 
 app = Flask(__name__)
 
+# Global Calendar URL to get from Google
 CALENDAR_URL = "https://www.googleapis.com/calendar/v3/calendars/{}/events".format(CALENDAR_ID)
+
+# Global caching
 CALENDAR_CACHED = None
 EVENTS_CACHED = None
 
 
-def cache_calendar(calendar_url, params=None):
+def cache_calendar_events(calendar_url, params=None):
     """Attempt to connect to the calendar and fetch the JSON provided."""
     try:
         global CALENDAR_CACHED
         CALENDAR_CACHED = requests.get(calendar_url, params=params).json()
     except ConnectionError:
+        # Can't connect, are we connected to the internet?
         return False
     return CALENDAR_CACHED
 
 
-def get_calendar(calendar_url, params=None):
-    """Return cache or set a new cache on the calendar for today"""
-    return cache_calendar(calendar_url, params=params)
+def get_calendar_events(calendar_url, params=None):
+    """Ask Google for new events from the calendar"""
+    return cache_calendar_events(calendar_url, params=params)
     # return CALENDAR_CACHED or cache_calendar(calendar_url)
 
 
-def get_calendar_today(CALENDAR_URL):
+def get_calendar_events_today(calendar_url):
+    """Get the calendar events from only today. 24 hour span from midnight."""
+    # Access the global cache
+    global EVENTS_CACHED
+
+    # Construct today and tomorrow datetime objects
     today = datetime.datetime.today()
     tomorrow = today
     tomorrow += datetime.timedelta(days=1)
-    cal = get_calendar(CALENDAR_URL, params={'key': API_KEY, 'singleEvents': True,
-                                             'timeMax': tomorrow.strftime("%Y-%m-%dT00:00:00-06:00"),
-                                             'timeMin': today.strftime("%Y-%m-%dT00:00:00-06:00")})
-    # today = datetime.datetime.now().date()
-    # for event_iter, event in enumerate(cal['items']):
-    # if 'start' in event and 'dateTime' in event['start']:
-    #         event_start_time = convert_from_iso(event['start']['dateTime']).date()
-    #         if today == event_start_time:
-    #             events.append(event)
-    global EVENTS_CACHED
+
+    # Get the calendar events subjective to time and singleEvents which expands recurring events
+    cal = get_calendar_events(calendar_url, params={'key': API_KEY, 'singleEvents': True,
+                                                    'timeMax': tomorrow.strftime("%Y-%m-%dT00:00:00-06:00"),
+                                                    'timeMin': today.strftime("%Y-%m-%dT00:00:00-06:00")})
+
+    # Specifically, events are held in the items array
     EVENTS_CACHED = cal['items']
     return EVENTS_CACHED
 
 
 def convert_from_iso(s):
+    """Convert from ISO 8601, worst date format, ever...Especially since Google ONLY does this."""
+    # TODO: Allow for more timezones than just -6 GMT
     return datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S-06:00")
 
 
 def find_event_property(properties, value, events):
+    """Returns true if a property with a value was found within the events list, else false."""
     for event in events:
         if properties in event and event[properties] == value:
             return True
@@ -59,50 +89,63 @@ def find_event_property(properties, value, events):
 
 
 def compare_events(events_prev, events_new):
-    for event in events_prev:
-        if 'start' in event and not find_event_property('start', event['start'], events_new) or \
-                                'end' in event and not find_event_property('end', event['end'], events_new) or \
-                                'summary' in event and not find_event_property('summary', event['summary'],
-                                                                               events_new) or \
-                                'location' in event and not find_event_property('location', event['location'],
-                                                                                events_new) or \
-                                'description' in event and not find_event_property('description', event['description'],
-                                                                                   events_new):
-            return False
+    """Compare two event arrays to determine if they have lost/gained anything significant since last check."""
+    # Fields we are interested in monitoring
+    fields = ['start', 'end', 'summary', 'location', 'description']
+
+    # The most obvious check is just the length of the previous event list, and the current.
+    # If event length has changed, then we need to update.
     if len(events_prev) != len(events_new):
         return False
+
+    # Scan all previous events and compare attributes to current
+    for event in events_prev:
+        for field in fields:
+
+            # If the field exists in some event, then it likely didn't change
+            # TODO: This is invalid if two fields have the same field/value and then one changes.
+            if field in event and not find_event_property(field, event[field], events_new):
+                # No match found, something has changed.
+                return False
+
+    # All checks done, nothing has changed since our last check
     return True
 
 
 @app.template_filter('str_hour')
 def str_hour_to_hour(s):
+    """Template filter that converts ISO format to meaningful Hour:Minute A.M/P.M format"""
     # TODO: Fix the -06:00 time zone offset
     if s:
         d = convert_from_iso(s)
         return datetime.datetime.strftime(d, "%I:%M %p").lstrip('0').strip(" ")
     else:
+        # Couldn't parse, return original.
         return s
 
 
 @app.route('/')
 def index():
-    events = get_calendar_today(CALENDAR_URL)
-    return render_template('index.html', events=events)
+    """Return the template with the event calendar"""
+    return render_template('index.html', events=get_calendar_today(CALENDAR_URL))
 
 
 @app.route('/events/')
 def events():
+    """Queries Google and asks for update on calendar"""
+    # Compare cache against a new GET request
     temp_cache = EVENTS_CACHED
-    events = get_calendar_today(CALENDAR_URL)
+    events_new = get_calendar_events_today(CALENDAR_URL)
 
-    if temp_cache is None or compare_events(temp_cache, events):
-        print "NO CHANGE IN EVENTS!"
+    # If not change is detected, tell the browser to keep it's current content.
+    if temp_cache is None or compare_events(temp_cache, events_new):
         return "false"
 
-    print "CHANGE DETECTED IN EVENTS!"
-    return render_template('events.html', events=events)
+    # Else, render the partial events template to return to the client.
+    return render_template('events.html', events=events_new)
 
-
+# Let's go!
 if __name__ == '__main__':
+    # Debug always helps
     app.debug = True
     app.run()
