8a90fbde98d2b593e86ba7a1ffdbbe0d7f5af3dc
Kyle Richelhoff <kyle@mythcomputers.ca>
2015-03-23 16:57:14 -0600

    Fix everything

--- StartupEdmontonBillBoard.py
+++ StartupEdmontonBillBoard.py
@@ -75,7 +75,55 @@ def get_calendar_events_today(calendar_url):
     return EVENTS_CACHED
 
 
-def get_calendar_events_limit(calendar_url, limit=10):
+def sort_events_days(events):
+    """Sort the event into an array of events by day"""
+    # Last event used in the loop
+    last_event = datetime.datetime.now()
+
+    # The day we are currently working with
+    current_day = datetime.datetime.now()
+
+    # Event days such that each day is its own index in the array of events
+    # ie. [[today], [tomorrow], [day after], [etc]]
+    event_days = []
+
+    # The list of events for the current day
+    day = []
+    for i, event in enumerate(events, start=1):
+        if convert_from_iso(event['start']['dateTime']).day == last_event.day:
+            # This is an event for today
+            day.append(event)
+
+            # If this is the last iteration of the loop, we should append our day before breaking
+            if i == len(events):
+                event_days.append(day)
+        else:
+            # This event is the start of a new day, so we need to add it.
+            event_days.append(day)
+
+            # TODO: Really bad while loop
+            # While this event is not the next day, increment the array
+            while True:
+                # Clear our current day in favour of a new one
+                day = []
+
+                # Increment our current day until we find an event that matches it
+                current_day = current_day + datetime.timedelta(days=1)
+                if current_day.day == convert_from_iso(event['start']['dateTime']).day:
+                    day.append(event)
+                    break
+                else:
+                    # This event and all other events are not in this day, so it needs to be blank
+                    event_days.append(day)
+
+        # Last event in the loop for comparison against future events
+        last_event = convert_from_iso(event['start']['dateTime'])
+
+    # Event days sorted
+    return event_days
+
+
+def get_calendar_events_limit(calendar_url, limit=10, sort=True):
     """Get the calendar events from only today. 24 hour span from midnight."""
     # Access the global cache
     global EVENTS_CACHED
@@ -86,11 +134,15 @@ def get_calendar_events_limit(calendar_url, limit=10):
     # Get the calendar events subjective to time and singleEvents which expands recurring events
     cal = get_calendar_events(calendar_url, params={'key': API_KEY, 'singleEvents': True,
                                                     'orderBy': 'startTime',
-                                                    'timeMin': today.strftime("%Y-%m-%dT00:00:00-06:00")})
+                                                    'timeMin': today.strftime("%Y-%m-%dT00:00:00-06:00"),
+                                                    'maxResults': limit})
 
     # Specifically, events are held in the items array
     EVENTS_CACHED = cal['items']
-    return EVENTS_CACHED[:limit]
+    if sort:
+        return sort_events_days(EVENTS_CACHED)
+    else:
+        return EVENTS_CACHED
 
 
 def convert_from_iso(s):
@@ -143,11 +195,35 @@ def str_hour_to_hour(s):
         return s
 
 
+@app.template_filter('str_day')
+def str_day(s):
+    """Template filter that converts ISO format to meaningful day format"""
+    # TODO: Fix the -06:00 time zone offset
+    if s:
+        d = convert_from_iso(s)
+        return datetime.datetime.strftime(d, "%d").strip(" ")
+    else:
+        # Couldn't parse, return original.
+        return s
+
+
+@app.template_filter('str_day_month')
+def str_day_month(s):
+    """Template filter that converts ISO format to meaningful month format"""
+    # TODO: Fix the -06:00 time zone offset
+    if s:
+        d = convert_from_iso(s)
+        return datetime.datetime.strftime(d, "%B %d|%A").strip("0")
+    else:
+        # Couldn't parse, return original.
+        return s
+
+
 @app.route('/')
 def index():
     """Return the template with the event calendar"""
     # return render_template('index.html', events=get_calendar_events_today(CALENDAR_URL))
-    return render_template('index.html', events=get_calendar_events_limit(CALENDAR_URL))
+    return render_template('index.html', events=get_calendar_events_limit(CALENDAR_URL), events_sorted=True)
 
 
 @app.route('/events/')
@@ -156,14 +232,14 @@ def events():
     # Compare cache against a new GET request
     temp_cache = EVENTS_CACHED
     # events_new = get_calendar_events_today(CALENDAR_URL)
-    events_new = get_calendar_events_limit(CALENDAR_URL)
+    events_new = get_calendar_events_limit(CALENDAR_URL, sort=False)
 
     # If not change is detected, tell the browser to keep it's current content.
     if temp_cache is None or compare_events(temp_cache, events_new):
         return "false"
 
     # Else, render the partial events template to return to the client.
-    return render_template('events.html', events=events_new)
+    return render_template('events_sorted.html', events=sort_events_days(events_new))
 
 # Let's go!
 if __name__ == '__main__':
